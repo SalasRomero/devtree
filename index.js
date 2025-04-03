d5377591d4a7cdf37087fbc2c1b9deb1956653c7
David Salas <dasa.19983@gmail.com>
2025-04-03 00:06:54 -0500

    feat, primeras rutas express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hola mundo desde devtree");
+const express = require('express')
+
+const app = express();
+
+//Routing
+app.get('/',(req,res)=>{
+    res.send('Hola mundo en express');
+});
+
+
+app.get('/ecommers',(req,res)=>{
+    res.send('Este es el ecommers');
+});
+
+app.get('/blog',(req,res)=>{
+    res.send('Este es el blog');
+});
+
+
+
+app.listen(4000,()=> console.log("servidor funcionando"))
