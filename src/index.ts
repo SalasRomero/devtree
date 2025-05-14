39f93c675fec6b08d7af95f7bdb6487891f2816e
David Salas <dasa.19983@gmail.com>
2025-05-13 23:25:52 -0500

    separando el servidor en un archivo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import express from 'express';
-
-const app = express();
-
-//Routing
-app.get('/',(req,res)=>{
-    res.send('Hola mundo en express / TypeScript');
-});
+import server from './server';
 
 const port = process.env.port || 4000;
-app.listen(port,()=> {
+server.listen(port,()=> {
     console.log("servidor funcionando en el puerto",port)
 })
